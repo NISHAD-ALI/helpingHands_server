0c67e46bc03726bec6f50fff6a4f70472221a187
nishad <nishad1868@gmail.com>
2024-05-08 12:09:52 +0530

    new Push

--- src/frameworks/utils/mailGenerator.ts
+++ src/frameworks/utils/mailGenerator.ts
@@ -1,2 +1,38 @@
 import nodemailer from 'nodemailer'
-import 
+import InodeMailerInterface from '../../useCases/interfaces/InodeMailerInterface'
+import dotenv from 'dotenv'
+dotenv.config();
+
+class SendMail implements InodeMailerInterface{
+
+    private transporter;
+    constructor(){
+        this.transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth:{
+                user:process.env.EMAIL,
+                pass:process.env.PASSWORD
+            }
+        })
+    }
+    async sendMail(to: string, otp: string): Promise<any> {
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to,
+            subject: 'Verify Your Account',
+            text: `Your OTP for email verification is ${otp}`
+        }
+        
+        this.transporter.sendMail(mailOptions,(err)=>{
+            if(err){
+                console.log(err);
+            }else{
+                console.log('OTP sent successfully!')
+            }
+        });
+
+       
+    }
+}
+
+export default SendMail;
--- src/useCases/userUsecase.ts
+++ src/useCases/userUsecase.ts
@@ -3,35 +3,38 @@ import IUserInterface from "./interfaces/IUserInterface";
 import otpGenerator from "../frameworks/utils/otpGenerator";
 import jwt from 'jsonwebtoken';
 import Jwt from "../frameworks/utils/jwtAuth";
-class userUseCases{
-   private userRepo : IUserInterface;
-   private generateOtp : otpGenerator;
-   private jwt:Jwt
-   constructor(userRepo : IUserInterface,generateOtp : otpGenerator,jwt:Jwt){
-    this.userRepo = userRepo
-    this.generateOtp = generateOtp
-    this.jwt = jwt
-   }
-   async findUser(userData: user) {
-    try {
-        let userExist = await this.userRepo.findUserByEmail(userData.email)
-        if (userExist) {
-            return { data: true }
-        } else {
-            const otp = this.generateOtp.generateOTP();
-            console.log(otp);
-            let token = jwt.sign({ userData, otp }, process.env. as string, { expiresIn: '5m' });
-            await this.sendOtp.sendMail(userData.email, otp)
-            return {
-                data: false,
-                token: token
-            }
-        }
-    } catch (err) {
-        console.log(err);
-        throw err;
+import SendMail from "../frameworks/utils/mailGenerator";
+class userUseCases {
+    private userRepo: IUserInterface;
+    private generateOtp: otpGenerator;
+    private jwt: Jwt
+    private sendMailOtp: SendMail
+    constructor(userRepo: IUserInterface, generateOtp: otpGenerator, jwt: Jwt, sendMailOtp: SendMail) {
+        this.userRepo = userRepo
+        this.generateOtp = generateOtp
+        this.jwt = jwt
+        this.sendMailOtp = sendMailOtp
     }
-}
+    async findUser(userData: user) {
+        try {
+            let userExist = await this.userRepo.findUserByEmail(userData.email)
+            if (userExist) {
+                return { data: true }
+            } else {
+                const otp = this.generateOtp.generateOTP();
+                // console.log(otp);
+                let token = jwt.sign({ userData, otp }, process.env.JWT_SECRET_KEY as string, { expiresIn: '5m' });
+                await this.sendMailOtp.sendMail(userData.email, otp)
+                return {
+                    data: false,
+                    token: token
+                }
+            }
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+    }
 }
 
 export default userUseCases
